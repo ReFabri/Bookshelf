ddb10374320bc05cba31275f2c08d5d18f2b6ddd
Renato <renato.f.fabri@gmail.com>
2023-11-15 14:26:05 -0300

    ADD Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div>BookShelf</div>
+        <div>
+          <input type="text" name="" id="" placeholder="Search for a book" />
+        </div>
+        <div>
+          <button>Add Book</button>
+          <button>All Books</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
