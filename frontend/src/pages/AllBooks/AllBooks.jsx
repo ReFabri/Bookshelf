be4f59832db96c2e7f8b0ecc63b0520ce1dc9d40
Renato <renato.f.fabri@gmail.com>
2023-11-17 16:18:45 -0300

    ADD AllBooks page style

--- frontend/src/pages/AllBooks/AllBooks.jsx
+++ frontend/src/pages/AllBooks/AllBooks.jsx
@@ -1,3 +1,5 @@
+import "./AllBooks.css";
+
 const AllBooks = () => {
   const allBooks = [
     {
@@ -21,17 +23,30 @@ const AllBooks = () => {
   ];
 
   return (
-    <section>
-      <h1>All your books</h1>
-      {!allBooks.length ? (
-        <h2>No Books in your List</h2>
-      ) : (
-        allBooks.map((book) => (
-          <p key={book.id}>
-            {book.title} by {book.author}, year: {book.publishYear}
-          </p>
-        ))
-      )}
+    <section className="allBooks-section">
+      <div className="allBooks-container">
+        <h1>All your books</h1>
+        {!allBooks.length ? (
+          <div>
+            <h2>No Books in your List</h2>
+            <p>📕 Add books to your list at the top navigation bar</p>
+          </div>
+        ) : (
+          allBooks.map((book) => (
+            <div key={book.id} className="allBooks-index">
+              <div>
+                <span>{book.title}</span>
+              </div>
+              <div>
+                <span>{book.author}</span>
+              </div>
+              <div className="allBooks-year">
+                Year: <span>{book.publishYear}</span>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </section>
   );
 };
