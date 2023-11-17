056d5b81b9e37d75d5c9fe25c31be4870becc328
Renato <renato.f.fabri@gmail.com>
2023-11-17 14:48:34 -0300

    ADD AllBooks page layout

--- frontend/src/pages/AllBooks/AllBooks.jsx
+++ frontend/src/pages/AllBooks/AllBooks.jsx
@@ -1,5 +1,39 @@
 const AllBooks = () => {
-  return <div>AllBooks</div>;
+  const allBooks = [
+    {
+      id: 1,
+      title: "Super Amazing Book",
+      author: "Mister Amazing Author",
+      publishYear: 1234,
+    },
+    {
+      id: 2,
+      title: "Sci-Fi Nerdfest",
+      author: "Annoying Arrogant Nerd",
+      publishYear: 1234,
+    },
+    {
+      id: 3,
+      title: "Fantasy and Dragons",
+      author: "Adult Man-Child",
+      publishYear: 1234,
+    },
+  ];
+
+  return (
+    <section>
+      <h1>All your books</h1>
+      {!allBooks.length ? (
+        <h2>No Books in your List</h2>
+      ) : (
+        allBooks.map((book) => (
+          <p key={book.id}>
+            {book.title} by {book.author}, year: {book.publishYear}
+          </p>
+        ))
+      )}
+    </section>
+  );
 };
 
 export default AllBooks;
