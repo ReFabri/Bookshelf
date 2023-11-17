a0bdbe139dfdf0e491ca7e387f6b7abc7021d001
Renato <renato.f.fabri@gmail.com>
2023-11-16 21:31:14 -0300

    ADD AddBook page layout and style

--- frontend/src/pages/AddBook/AddBook.jsx
+++ frontend/src/pages/AddBook/AddBook.jsx
@@ -1,5 +1,20 @@
+import "./AddBook.css";
+
 const AddBook = () => {
-  return <div>AddBook</div>;
+  return (
+    <section className="addBook-section">
+      <div className="addBook-form">
+        <h1>Add a new book</h1>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="author">Author</label>
+        <input type="text" name="author" id="author" />
+        <label htmlFor="publishYear">Publish Year</label>
+        <input type="number" name="publishYear" id="publishYear" />
+        <button>Add Book</button>
+      </div>
+    </section>
+  );
 };
 
 export default AddBook;
