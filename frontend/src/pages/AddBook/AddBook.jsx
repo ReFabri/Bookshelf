ea723512da846c35ac5f75673cd9ca0ff03f92c5
Renato <renato.f.fabri@gmail.com>
2023-11-18 17:03:06 -0300

    ADD AddBook page POST request

--- frontend/src/pages/AddBook/AddBook.jsx
+++ frontend/src/pages/AddBook/AddBook.jsx
@@ -1,17 +1,52 @@
 import "./AddBook.css";
+import { useState } from "react";
 
 const AddBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+
+  const submitHandler = async () => {
+    try {
+      await fetch("http://localhost:5000/bookstore/", { method: "POST" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="addBook-section">
       <div className="addBook-form">
         <h1>Add a new book</h1>
+
         <label htmlFor="title">Title</label>
-        <input type="text" name="title" id="title" />
+        <input
+          value={title}
+          type="text"
+          name="title"
+          id="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
         <label htmlFor="author">Author</label>
-        <input type="text" name="author" id="author" />
+        <input
+          value={author}
+          type="text"
+          name="author"
+          id="author"
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+
         <label htmlFor="publishYear">Publish Year</label>
-        <input type="number" name="publishYear" id="publishYear" />
-        <button>Add Book</button>
+        <input
+          value={publishYear}
+          type="number"
+          name="publishYear"
+          id="publishYear"
+          onChange={(e) => setPublishYear(e.target.value)}
+        />
+
+        <button onClick={submitHandler}>Add Book</button>
       </div>
     </section>
   );
